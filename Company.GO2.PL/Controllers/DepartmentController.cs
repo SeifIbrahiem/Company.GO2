1ecc19b8a25070fb916bfc9644fcc8c21a05e308
Greg Low <greg@gmail.com>
2025-03-15 00:42:17 +0200

    final part 1

--- Company.GO2.PL/Controllers/DepartmentController.cs
+++ Company.GO2.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
 ﻿using Company.GO2.BLL.Repositories;
+using Company.GO2.DAL.Models;
+using Company.GO2.PL.DTOS;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.GO2.PL.Controllers
@@ -29,8 +31,27 @@ namespace Company.GO2.PL.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) //server side validation
+            {
+                var department = new Department()
+                {
+                   Code = model.Code,
+                   Name = model.Name,
+                   CreatedDate= model.CreateAt,
+                };
+                var count = _departmentRepositry.Add(department);
+                if (count > 0) 
+                { 
+                  return RedirectToAction(nameof(Index));
+                }
+            }
 
 
+            return View();
+        }
 
 
 
